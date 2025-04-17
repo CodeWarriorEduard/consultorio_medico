e705fcc019451a25d239c84cdf8a0927c4a45362
CodewarriorEduard <>
2025-04-17 14:37:47 -0500

    Services Appointment, Doctor and patient implementations,  exceptions and mappers: AppointmentMapper ConsultRoomMapper DoctorMapper and PatientMapper

--- src/main/java/com/rafael/consultorio_medico_actividad/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rafael/consultorio_medico_actividad/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.rafael.consultorio_medico_actividad.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(value = ResourceNotFoundException.class)
+//    public ResponseEntity<DefaultApiResponse<Object>> aerolineaNotFoundExceptionHandler(AerolineaNotFoundException e){
+//        DefaultApiResponse<Object> apiResponse = new DefaultApiResponse<>(
+//                "404 NOT FOUND",
+//                e.getMessage(),
+//                null,
+//                null
+//        );
+//        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+//    }
+
 }
